631e97a21c470796048b61eca156bb0b2596130e
unknown <jergcauso@gmail.com>
2023-04-27 15:15:34 +0800

    Initial

---
+++ 01-variable-datatypes/01console/script.js
@@ -0,0 +1,30 @@
+// alert("Hello from JS File");
+
+//log a number
+console.log(100);
+
+//log string
+console.log("Hello World");
+
+//log a variable
+
+//log multiple values
+console.log(20, "HEllo", true);
+
+//log a variable
+const x = 100;
+console.log(x);
+
+//console error & warning
+console.log("Alert");
+console.log("Warning");
+
+//log object as a tablw
+console.table({ name: "Ryan", email: "jergcauso@gmail.com" });
+
+//group console commands
+console.group("simple");
+console.log(x);
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
