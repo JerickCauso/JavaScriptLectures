867b5978d38a0166d161de576939fe445eb1e36b
unknown <jergcauso@gmail.com>
2023-04-27 15:37:19 +0800

    Inital Comment

---
+++ 02comments-shortcuts/script.js
@@ -0,0 +1,9 @@
+//single line of code comment
+
+//the shortcut key is: crtl + /
+
+// multi line Comment shortcut key: Shift + Alt + A
+
+console.log(100);
+console.log("Hello World");
+console.log(20, "Hello", true);
